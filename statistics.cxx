c3aa6d0c11c47ed30cc96f79e1fbd907aaddc43a
Nico Verbeek <nico.verbeek@hhu.de>
2015-11-14 13:01:09 +0100

    Blubb

--- statistics.cxx
+++ statistics.cxx
@@ -3,18 +3,47 @@
 #include <cstdlib>
 
 using namespace std;
-//--------------------------------------------------
-// plenty of lines here
-//--------------------------------------------------
+
+void randomnumbers( double* x, int N){
+
+   for (int i = 0; i < N; i++){
+   x[i] = rand() % 2;
+   }
+}
+
+void mean(double& m, int N){
+
+double p[N], sum = 0;
+randomnumbers(p, N);
+
+for (int i = 0; i < N; i++){ 
+	sum += p[i];
+}
+m = sum/N;
+}
+
+void variance(double& var, int N){
+
+double p[N], m, sum = 0;
+randomnumbers(p, N);
+mean(m, N);
+	
+for (int i = 0; i < N; i++){
+	sum += pow((p[i]-m),2.0);
+}
+var= sum/N;
+}
 
 int main(){
-   const int N = 100;
+   const int N = 100000;
    double p[N];
-   double mean, var;
-
-   // Some lines here....
-
-   cout << "Mean = " << mean << endl;
+   double m, var;
+  
+   randomnumbers(p, N);
+   mean(m, N);
+   variance(var, N);
+   
+   cout << "Mean = " << m << endl;
    cout << "Variance = " << var << endl;
 
    return 0;
